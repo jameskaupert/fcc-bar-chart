b726b08375ac43fcd8a4c8bc668c467f875e6828
James Kaupert <jameskaupert@gmail.com>
2018-01-05 05:44:16 -0500

    set the width and height appropriately

--- public/js/barChart.js
+++ public/js/barChart.js
@@ -1,32 +1,47 @@
-let json;
 let req = new XMLHttpRequest();
-req.open("GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
+req.open(
+  "GET",
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json",
+  true
+);
 req.send();
 req.onload = function() {
-    json = JSON.parse(req.responseText);
-    console.log(json)
-}
+  const json = JSON.parse(req.responseText);
+  const dataset = json["data"];
+  console.log(dataset);
 
+  const yMin = d3.min(dataset, d => d[1]);
+  const yMax = d3.max(dataset, d => d[1]);
 
+  console.log('yMin', yMin, 'yMax', yMax)
 
+  const w = 963;
+  const h = 500;
+  const padding = 20;
 
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, dataset.length])
+    .range([padding, w - padding]);
+  const yScale = d3
+    .scaleLinear()
+    .domain([yMin, yMax])
+    .range([h - padding, padding]);
 
+  const svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
-
-    const w = 500;
-    const h = 100;
-
-    const svg = d3.select(".container")
-                  .append("svg")
-                  .attr("width", w)
-                  .attr("height", h);
-
-    svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", (d, i) => i * 30)
-       .attr("y", (d, i) => h - 3 * d)
-       .attr("width", 25)
-       .attr("height", (d, i) => 3 * d)
+  svg
+    .selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => i * 3.5)
+    .attr("y", (d, i) => yScale(d[1]))
+    .attr("width", 2.5)
+    .attr("height", (d, i) => h - yScale(d[1]))
+    .attr("class");
+};
